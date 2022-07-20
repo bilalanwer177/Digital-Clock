ecb44490cc8afdcf9e258377f41264097fd42396
Bilal Anwer <bilalanwer177@gmail.com>
2022-07-20 18:42:34 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ const App = () => {
   const UpdateTime = () => {
     time = new Date().toLocaleTimeString();
     setCtime(time);
-  }
+  } 
   setInterval(UpdateTime, 1000)
-} 
   return (
     <>
       <div className="container" >
         <div className="heading">
           <h1> Current Time </h1>
         </div>
-
         <h1 className="text" > {ctime} </h1>
       </div>
     </>
   )
+}
 export default App;
